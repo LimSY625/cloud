ad93ad0de6c387da69b4ef3801c76576b3e9bcf1
LimSY625 <limsuu0625@gmail.com>
2021-08-11 16:48:33 +0900

    cloud_class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import '@cds/core/button/register.js';
+import { CdsModule } from '@cds/angular';
+import {ClarityModule} from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CdsModule,
+    ClarityModule,
+    BrowserAnimationsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
